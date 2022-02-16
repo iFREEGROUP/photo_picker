fbd3a2cb5324179fd6096a7a27e335a2de2e193f
lamandys <lamandys@ifreegroup.com>
2022-02-16 11:10:04 +0800

    feat: 优化相册目录名称

--- lib/delegates/photo_rename_delegate.dart
+++ lib/delegates/photo_rename_delegate.dart
@@ -5,36 +5,82 @@ abstract class PhotoNameDelegate {
 
   static PhotoNameDelegate defaultPhotoNameDelegate =
       DefaultPhotoNameDelegate();
+
+  bool isChinese(BuildContext context) =>
+      Localizations.localeOf(context).languageCode == 'zh';
 }
 
 class DefaultPhotoNameDelegate extends PhotoNameDelegate {
   @override
   String rename(BuildContext context, String origin) {
-    if (!isChinese(context)) return origin;
     String result = origin;
+    if (!isChinese(context)) {
+      switch (origin) {
+        case '最近项目':
+        case '最近':
+        case '全部':
+        case '所有':
+          result = 'Recent';
+          break;
+        case '图片':
+        case '照片':
+        case '图库':
+          result = 'Pictures';
+          break;
+        case '相机':
+        case '拍照':
+        case '相機':
+        case '原況照片':
+        case '实况照片':
+          result = 'Camera';
+          break;
+        case '截圖':
+        case '熒幕截圖':
+        case '截屏':
+          result = 'Screenshots';
+          break;
+        case '视频':
+          result = 'Video';
+          break;
+        case '电影':
+          result = 'Movie';
+          break;
+        default:
+      }
+      return result;
+    }
     switch (origin) {
       case 'Recent':
+      case 'All':
         result = '最近';
         break;
       case 'Pictures':
+      case 'Picture':
+      case 'picture':
+      case 'pictures':
         result = '图片';
         break;
       case 'Camera':
+      case 'camera':
+      case 'Live Photos':
         result = '相机';
         break;
       case 'Screenshots':
       case 'Screenshot':
+      case 'screenshots':
+      case 'screenshot':
         result = '截图';
         break;
       case 'video':
       case 'Video':
         result = '视频';
         break;
+      case 'Movie':
+      case 'movie':
+        result = '电影';
+        break;
       default:
     }
     return result;
   }
-
-  bool isChinese(BuildContext context) =>
-      Localizations.localeOf(context).languageCode == 'zh';
 }
--- lib/delegates/photo_sort_path_delegate.dart
+++ lib/delegates/photo_sort_path_delegate.dart
@@ -6,6 +6,30 @@ abstract class SortPathDelegate<Path> {
   void sort(List<Path> list);
 
   static const defaultDelegate = DefaultSortPathDelegate();
+
+  bool isCamera(AssetPathEntity entity) {
+    return entity.name == 'Camera' ||
+        entity.name == '相机' ||
+        entity.name == '相機' ||
+        entity.name == 'Live Photos' ||
+        entity.name == '原況照片' ||
+        entity.name == '实况照片';
+  }
+
+  bool isScreenShot(AssetPathEntity entity) {
+    return entity.name == 'Screenshots' ||
+        entity.name == 'Screenshot' ||
+        entity.name == '截圖' ||
+        entity.name == '熒幕截圖' ||
+        entity.name == '截屏';
+  }
+
+  bool isHide(AssetPathEntity entity) {
+    return entity.name == '已隐藏' ||
+        entity.name == 'Hidden' ||
+        entity.name == '非表示' ||
+        entity.name == '已隱藏';
+  }
 }
 
 class DefaultSortPathDelegate extends SortPathDelegate<AssetPathEntity> {
@@ -20,16 +44,16 @@ class DefaultSortPathDelegate extends SortPathDelegate<AssetPathEntity> {
       if (path2.isAll) {
         return 1;
       }
-      if (_isCamera(path1)) {
+      if (isCamera(path1)) {
         return -1;
       }
-      if (_isCamera(path2)) {
+      if (isCamera(path2)) {
         return 1;
       }
-      if (_isScreenShot(path1)) {
+      if (isScreenShot(path1)) {
         return -1;
       }
-      if (_isScreenShot(path2)) {
+      if (isScreenShot(path2)) {
         return 1;
       }
       return 0;
@@ -37,35 +61,11 @@ class DefaultSortPathDelegate extends SortPathDelegate<AssetPathEntity> {
 
     // 删除隐藏相册
     list.removeWhere((element) {
-      return _isHide(element);
+      return isHide(element);
     });
   }
 
   int otherSort(AssetPathEntity path1, AssetPathEntity path2) {
     return path1.name.compareTo(path2.name);
-  }
-
-  bool _isCamera(AssetPathEntity entity) {
-    return entity.name == 'Camera' ||
-        entity.name == '相机' ||
-        entity.name == '相機' ||
-        entity.name == 'Live Photos' ||
-        entity.name == '原況照片' ||
-        entity.name == '实况照片';
-  }
-
-  bool _isScreenShot(AssetPathEntity entity) {
-    return entity.name == 'Screenshots' ||
-        entity.name == 'Screenshot' ||
-        entity.name == '截圖' ||
-        entity.name == '熒幕截圖' ||
-        entity.name == '截屏';
-  }
-
-  bool _isHide(AssetPathEntity entity) {
-    return entity.name == '已隐藏' ||
-        entity.name == 'Hidden' ||
-        entity.name == '非表示' ||
-        entity.name == '已隱藏';
   }
 }
